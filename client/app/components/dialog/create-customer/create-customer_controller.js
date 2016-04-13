0f9a39a0eb15e92f9f7d20affbb7ce8864af71df
Ankush Verma <vankush1990@gmail.com>
2016-04-13 15:27:09 +0530

    done

--- client/app/components/dialog/create-customer/create-customer_controller.js
+++ client/app/components/dialog/create-customer/create-customer_controller.js
@@ -16,17 +16,50 @@
       $mdDialog.cancel();
     };
      $scope.formData={};
-     $scope.confirm = function(){
+     $scope.formData.email=$scope.email;
+
+
+     $scope.confirm = function(formData){
        var formData=$scope.formData;
+       $scope.formData.email=$scope.email;
+       $scope.formData.first_name=$scope.first_name;
+       $scope.formData.last_name=$scope.last_name;
+       $scope.formData.ip=$scope.ip;
+       $scope.formData.latitude=$scope.latitude;
+       $scope.formData.longitude=$scope.longitude;
+
        $scope.formData.created_at=new Date();
        $scope.formData.updated_at=new Date();
-       myService.addCustomer(formData)
+        if($scope.email==undefined){
+      appPopupFactory.showSimpleToast('Please enter email');
+        }
+        if($scope.first_name==undefined){
+      appPopupFactory.showSimpleToast('Please enter first name');
+        }
+        if($scope.last_name==undefined){
+      appPopupFactory.showSimpleToast('Please enter last name');
+        }
+        if($scope.ip==undefined){
+      appPopupFactory.showSimpleToast('Please enter ip');
+        }
+        if($scope.latitude==undefined){
+      appPopupFactory.showSimpleToast('Please enter latitude');
+        }
+        if($scope.longitude==undefined){
+      appPopupFactory.showSimpleToast('Please enter longitude');
+        }
+       else{
+          myService.addCustomer(formData)
          .then(function() {
+          
            appPopupFactory.showSimpleToast('Customer created Successfully.');
+
            $state.go($state.current, {}, {reload: true});
 
            $scope.close();
          });
+         
+      }
 
     };
   }
